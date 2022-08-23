720985bec71a87e4ab30d0d5a4e310ffdd14db1b
Felix Avelar <=>
2022-08-23 16:48:13 -0600

    :sparkles: added cron schedule to clear sessions

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
 import { Bot } from './bot';
+import { createClient } from 'redis';
+import cron from 'node-cron';
+
+function clearSessions() {
+  const redis = createClient();
+  redis.connect();
+  redis.on('error', () => console.error('Redis Error'));
+  redis.flushDb();
+  console.log('Sessions cleared!')
+}
+
+cron.schedule('59 23 * * *', clearSessions); // Clears sessions every day at 11:59 pm
 
 const bot = new Bot()
 bot.start();
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,6 @@ export { API } from './API';
 
 export function setExpiration() {
   const time = new Date().getTime();
-  const expiration = 3600 * 3 * 1000; // + three hours
+  const expiration = 3600 * 2 * 1000; // +2 hours
   return time + expiration;
 }
